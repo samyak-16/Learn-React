1cd78808d7a530d9ae21d35209782672da9e4964
samyak-16 <samexecodes@gmail.com>
2025-09-02 20:16:35 +0545

    Introduction to React

---
+++ Learn React/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
