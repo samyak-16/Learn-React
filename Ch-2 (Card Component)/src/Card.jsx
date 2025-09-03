95a7044f0f477182525ec5b02453596f879b2d0d
samyak-16 <samexecodes@gmail.com>
2025-09-03 20:05:55 +0545

    Made a simple Card Component

---
+++ Ch-2 (Card Component)/src/Card.jsx
@@ -0,0 +1,19 @@
+function Card() {
+  return (
+    <>
+      <div className='card'>
+        <img
+          className='card-img'
+          src='https://scontent.fbdp2-1.fna.fbcdn.net/v/t1.6435-9/195708605_1919245488241281_1533730674894063524_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=UY-tzChU5KAQ7kNvwEyRPD6&_nc_oc=AdmzhmLvy3cC4_IWLS76sx9VxSAe65W1GWjc35NeQnTB0ESQHm3JB_fldQaHr1J4kAGjVR3OznFD1H5bYh2UU3RO&_nc_zt=23&_nc_ht=scontent.fbdp2-1.fna&_nc_gid=T39jgfDJAazS2IWTbalJbA&oh=00_AfZIVg4d1JAsNbHXDZNQ7Fd1-nulggbQPdCGm_LiPY1TaA&oe=68DF9886'
+          alt='Profile Picture'
+        />
+        <h2 className='card-title'>Samyak</h2>
+        <p className='card-text'>
+          Yoo Im samyak You better know me by my name right ?{' '}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Card;
